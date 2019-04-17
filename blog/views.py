4d68b7d5a32c5e7bfd31961ad2b4e008490739d0
nik54a <nik54a@mail.ru>
2019-04-17 19:40:06 +0300

    My Django Girls app, first commit

---
+++ blog/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+ 
+from  blog.models import Post
+ 
+ 
+def home(request):
+    postList = Post.objects.filter(visible='1')
+    paginator = Paginator(postList, 2)
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
+ 
+    context = {
+        "posts": posts,
+        "title": "Главная страница блога",
+        "desc": "Описание для главной страницы",
+        "key": "ключевые, слова",
+    }
+    return render(request, "partial/home.html", context)
+ 
+def single(request, id=None):
+    post = get_object_or_404(Post, id=id)
+ 
+    context = {
+        "post": post,
+    }
+    return render(request, "partial/single.html", context)
